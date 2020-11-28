dace9e70fac92b78295ec8462399e922d21f0786
Giovanni Salinas <gsalinaslopez@gmail.com>
2020-11-28 18:58:48 +0800

    2018 checkpoint

--- .p10k.zsh
+++ .p10k.zsh
@@ -48,7 +48,7 @@
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
     status                  # exit code of the last command
-    command_execution_time  # duration of the last command
+    # command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
